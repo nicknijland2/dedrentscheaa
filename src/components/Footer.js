0eff5ce3ff796713cadbc90f6451f2c01ec7c666
Nick Nijland <n.a.nijland.2005@gmail.com>
2021-11-04 21:55:54 +0100

    footer removed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -102,16 +102,7 @@ return(
        </Grid>
        <Grid alignItems="center" justifyContent="center" container spacing={3}>
            
-           <Grid item><Link style={{fontFamily:'Mulish,cursive'}} href="" color="inherit" onClick={()=> handleRoute('/')}>Home</Link></Grid>
-           <Grid item><Link style={{fontFamily:'Mulish,cursive'}} href="" color="inherit" onClick={()=> handleRoute('/about')}>Over ons </Link></Grid>
-           <Grid item><Link style={{fontFamily:'Mulish,cursive'}} href="" color="inherit" onClick={()=> handleRoute('/management')}>Bestuur</Link></Grid>
-           <Grid item><Link style={{fontFamily:'Mulish,cursive'}} href="" color="inherit" onClick={()=> handleRoute('/lessons')}>Lessen</Link>  </Grid>
-           <Grid item><Link style={{fontFamily:'Mulish,cursive'}} href="" color="inherit" onClick={()=> handleRoute('/members')}>Lidmaatschap</Link>  </Grid>
-           <Grid item><Link style={{fontFamily:'Mulish,cursive'}} href="" color="inherit" onClick={()=> handleRoute('/agenda')}>Agenda</Link>   </Grid>
-           <Grid item><Link style={{fontFamily:'Mulish,cursive'}} href="" color="inherit" onClick={()=> handleRoute('/rules')}>Reglement</Link>   </Grid>
-           <Grid item><Link style={{fontFamily:'Mulish,cursive'}} href="" color="inherit" onClick={()=> handleRoute('/sponsors')}>Sponsoren</Link>   </Grid>
-           <Grid item><Link style={{fontFamily:'Mulish,cursive'}} href="" color="inherit" onClick={()=> handleRoute('/contact')}>Contact</Link></Grid>
-           
+          
        </Grid>
        <Grid alignItems="center" justifyContent="center" container spacing={3} style={{marginTop:'2em'}}>
             <Typography gutterBottom variant="body1">
